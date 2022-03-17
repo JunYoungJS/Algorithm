2011887494f9370556a62516855532ac3bdccd05
JunYoungJS <ktsdsd94@gmail.com>
2022-03-17 15:32:45 +0900

    소문난칠공주 내풀이

---
+++ Baekjoon/BackTraking/소문난칠공주/1941(참조).js
@@ -0,0 +1,6 @@
+let dx=[0,0,1,-1]
+let dy=[1,-1,0,0]
+
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+//let input = fs.readFileSync(__dirname+'/input.txt').toString().split('\n');
