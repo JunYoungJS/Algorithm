099fbc88f9dbf6dc98bbfe60cc65a73dc70d8670
JunYoungJS <ktsdsd94@gmail.com>
2022-03-22 15:13:24 +0900

    solve 추석트래픽

--- Programmers/kakao/level3/추석트래픽.js
+++ Programmers/kakao/level3/추석트래픽.js
@@ -1,20 +1,38 @@
+// https://velog.io/@longroadhome/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-LV.3-%EC%B6%94%EC%84%9D-%ED%8A%B8%EB%9E%98%ED%94%BD-JS 참조 
+
 function solution (lines) {
     const times = [];
 
-
+    answer=0
     // 1. 주어진 구간 을 초로 변환 시킴 
     for(const line of lines) {
         let[days,time,duration]=line.split(' ')
         // 밀단위
         let endtime=getSecondtoMill(time)
+        
+    
         let start_time=getStartTimetoMill(duration,endtime)+1
+        //  마지막 구간에 +999를 해서 초당 처리량에 해당되는 범위를 전부 잡을수있음 
+        endtime+=999
 
 
 
-        console.log(start_time,endtime)
-
+        times.push(["Start",start_time])
+        times.push(["End",endtime])
     }
 
+    times.sort(sorting)
+    cnt=0
+    for(let i=0;i<times.length;i++){
+        if(times[i][0]=='Start'){
+            cnt+=1
+        }
+        else{
+            cnt-=1
+        }
+        answer=Math.max(cnt,answer)
+    }
+    return answer
  }
 
  const getSecondtoMill=(time)=>{
@@ -31,8 +49,25 @@ function solution (lines) {
      return time-number
  }
     
+const sorting=(a,b)=>{
+    
+    if(a[1]==b[1]){
+        return -1
+    }
+    else{
+        return a[1]-b[1]
+    }
+}
 
 
-solution([
-    '2016-09-15 03:10:33.020 0.011s'
-    ])
+
+
+//   solution([
+//     "2016-09-15 01:00:04.002 2.0s",
+//     "2016-09-15 01:00:07.000 2s"
+//     ])
+
+// solution([
+//     "2016-09-15 01:00:04.001 2.0s",
+//     "2016-09-15 01:00:07.000 2s"
+//     ])
