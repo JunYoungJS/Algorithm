936beaa683e44af6dd47b900707644fc691e44bd
JunYoungJS <ktsdsd94@gmail.com>
2022-06-06 18:25:59 +0900

    solve leetcode

--- LeetCode/Easy/javascript/Intersection of Two Linked Lists.js
+++ LeetCode/Easy/javascript/Intersection of Two Linked Lists.js
@@ -11,6 +11,10 @@ function ListNode(val) {
 var getIntersectionNode = function (headA, headB) {
   let a = headA;
   let b = headB;
+  
+    // headA가 더이상 next하지못하면 headB의 헤드로 연결해주고
+    // headB가 더이상 next하지못하면 headA의 헤드로 연결
+    // 둘이 같은지점이 생기면 교차점
 
   while (a !== b) {
     a = !a ? headB : a.next;
